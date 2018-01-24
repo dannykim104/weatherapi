b649bda44f7752373a4823270f6f699fda61984d
Daniel Kim <dannykim104@gmail.com>
2018-01-24 10:51:04 -0800

    init

---
+++ myWeatherApp/src/app/washintondc/washintondc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-washintondc',
+  templateUrl: './washintondc.component.html',
+  styleUrls: ['./washintondc.component.css']
+})
+export class WashintondcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
